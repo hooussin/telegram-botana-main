fce32ef2334a00acfd592d460392de4cefd0f6fe
hooussin <houssinpubg11111@gmail.com>
2025-07-19 02:51:58 +0100

    Update cash_transfer.py

--- handlers/cash_transfer.py
+++ handlers/cash_transfer.py
@@ -1,7 +1,7 @@
 from telebot import types
 from services.wallet_service import add_purchase, has_sufficient_balance
 from config import ADMIN_MAIN_ID
-from handlers.wallet import register_user_if_not_exist
+from services.wallet_service import register_user_if_not_exist
 from handlers import keyboards
 import math  # لإدارة صفحات الكيبورد
 import logging
